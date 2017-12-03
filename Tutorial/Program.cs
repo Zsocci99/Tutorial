f53ff68f66991abee057b8b36000217baf573781
Zsocci99 <zsoltcsokasijr@gmail.com>
2017-12-03 23:48:33 +0100

    Fixing

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -14,34 +14,35 @@ namespace Tutorial
             int second = 0;
             //delet |
             //      V
-            first = 1;
-            second = 2;
+            first = 3;
+            second = 1;
             Console.WriteLine("Variables: 1");
             //first = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             first:
-            switch (first)
+            switch (second)
             {
                 case 1:
                     Console.WriteLine("Bool: 1");
-                    Console.WriteLine("Char: 2");
-                    Console.WriteLine("Decimal: 3");
-                    Console.WriteLine("Double: 4");
-                    Console.WriteLine("Enum: 5");
-                    Console.WriteLine("Float: 6");
-                    Console.WriteLine("Int: 7");
-                    Console.WriteLine("Long: 8");
-                    Console.WriteLine("Sbyte: 9");
-                    Console.WriteLine("Short: 10");
-                    Console.WriteLine("Struct: 11");
-                    Console.WriteLine("Uint: 12");
-                    Console.WriteLine("Ulong: 13");
-                    Console.WriteLine("Ushort: 14");
+                    Console.WriteLine("Byte: 2");
+                    Console.WriteLine("Char: 3");
+                    Console.WriteLine("Decimal: 4");
+                    Console.WriteLine("Double: 5");
+                    Console.WriteLine("Enum: 6");
+                    Console.WriteLine("Float: 7");
+                    Console.WriteLine("Int: 8");
+                    Console.WriteLine("Long: 9");
+                    Console.WriteLine("Sbyte: 10");
+                    Console.WriteLine("Short: 11");
+                    Console.WriteLine("Struct: 12");
+                    Console.WriteLine("Uint: 13");
+                    Console.WriteLine("Ulong: 14");
+                    Console.WriteLine("Ushort: 15");
                 //second = Convert.ToInt32(Console.ReadLine());
                 second:
-                    switch (second)
+                    switch (first)
                     {
-                        case 1:
+                        case 1://Bool
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Bool:\n"); Console.ResetColor();
                             Console.WriteLine("A Bool egy logikai típusú változó.\nMérete 1 byte.");
@@ -49,7 +50,7 @@ namespace Tutorial
                             Console.WriteLine("Deklarálása:\nbool (NÉV) = (true/false);");
                             Console.WriteLine("PL:\nbool Boolean = true;\nBoolean = false;");
                             break;
-                        case 2:
+                        case 2://Byte
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Byte:\n"); Console.ResetColor();
                             Console.WriteLine("A Byte egy olyan szám tipusú változó amely 0 és 255 között képes tárolni számokat.\nMérete 1 byte");
@@ -57,7 +58,7 @@ namespace Tutorial
                             Console.WriteLine("Deklarálása:\nbyte (NÉV) = (0-255);");
                             Console.WriteLine("PL:\nbyte Byte = 99;\nByte = 0;");
                             break;
-                        case 3:
+                        case 3://Char
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Char:\n"); Console.ResetColor();
                             Console.WriteLine("A Char egy karakter típusú változó.\nMérete 2 byte.");
