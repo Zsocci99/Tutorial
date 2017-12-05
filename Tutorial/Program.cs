8cf16934b1933a019ad77bb5b685a21b286ed28b
Zsocci99 <zsoltcsokasijr@gmail.com>
2017-12-06 00:50:59 +0100

    Redirect

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -22,62 +22,115 @@ namespace Tutorial
             int S3 = 0;
             int S4 = 0;
             #endregion
+            S1:
+            Console.Clear();
+            Console.WriteLine("-C#-");
+            Console.WriteLine("Változók: 1");
+            Console.WriteLine("Vissza lépés: 0");
             S1 = Convert.ToInt32(Console.ReadLine());
             switch (S1)
             {
-                case 1:
-                    Console.WriteLine("Bool: 1");
-                    Console.WriteLine("Byte: 2");
-                    Console.WriteLine("Char: 3");
-                    Console.WriteLine("Decimal: 4");
-                    Console.WriteLine("Double: 5");
-                    Console.WriteLine("Enum: 6");
-                    Console.WriteLine("Float: 7");
-                    Console.WriteLine("Int: 8");
-                    Console.WriteLine("Long: 9");
-                    Console.WriteLine("Sbyte: 10");
-                    Console.WriteLine("Short: 11");
-                    Console.WriteLine("Struct: 12");
-                    Console.WriteLine("Uint: 13");
-                    Console.WriteLine("Ulong: 14");
-                    Console.WriteLine("Ushort: 15");
+                case 1://Változók
+                    S0:
+                    Console.Clear();
+                    Console.WriteLine("-C#-");
+                    Console.WriteLine("--Változók--");
+                    Console.WriteLine("Logical: 1");
+                    Console.WriteLine("Numeral: 2");
+                    Console.WriteLine("Betűs: 3");
+                    Console.WriteLine("Vissza lépés: 0");
                     S0 = Convert.ToInt32(Console.ReadLine());
                     switch (S0)
                     {
-                        case 1://Bool
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Bool:\n"); Console.ResetColor();
-                            Console.WriteLine("A Bool egy logikai típusú változó.\nMérete 1 byte.");
-                            Console.WriteLine("A logikai típus értéke a true vagy false értéket veheti fel.");
-                            Console.WriteLine("Deklarálása:\nbool (NÉV) = (true/false);");
-                            Console.WriteLine("PL:\nbool Boolean = true;\nBoolean = false;");
-                            break;
-                        case 2://Byte
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Byte:\n"); Console.ResetColor();
-                            Console.WriteLine("A Byte egy olyan szám tipusú változó amely 0 és 255 között képes tárolni számokat.\nMérete 1 byte");
-                            Console.WriteLine("A Byte 1 byton az az 8 biten tárol csak pozitív számokat(0-255).");
-                            Console.WriteLine("Deklarálása:\nbyte (NÉV) = (0-255);");
-                            Console.WriteLine("PL:\nbyte Byte = 99;\nByte = 0;");
-                            break;
-                        case 3://Char
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Char:\n"); Console.ResetColor();
-                            Console.WriteLine("A Char egy karakter típusú változó.\nMérete 2 byte.");
-                            Console.WriteLine("A char 16 bites karakterábrázolást (unikód) használ.");
-                            Console.WriteLine("A backslash (\\) karakter speciális jelentéssel bír. Az utána következő karakter(eke)t, mint egy escape szekvenciát dolgozza föl a fordító.Az így használható escape szekvenciák a következők:");
-                            Console.WriteLine("\\a - a 7-es kódú csipogás\n\\b - backspace, előző karakter törlése\n\\f - formfeed, soremelés karakter\n\\r - kocsi vissza karakter\n\\n - új sor karakter (soremelés + kocsi vissza)");
-                            Console.WriteLine("Az új sor karakter hatása azonos a formfeed és a kocsi vissza karakterekhatásával");
-                            Console.WriteLine("\\t - tabulátor karakter\n\\v - függőleges tabulátor\n\\\\ - backslash karakter\n\\' - aposztróf\n\\\" - idézőjel\n\\? - kérdőjel\n\\uxxyy xx és yy unikódú karakter");
-                            Console.WriteLine("Deklarálása: \n char (NÉV) = '(BÁRMELY KARAKTER';");
-                            Console.WriteLine("PL:\nchar Character = 'Z';\nCharacter = 'S';");
-                            break;
+                        case 1://Logical/ Bool
+                            {
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Bool:\n"); Console.ResetColor();
+                                Console.WriteLine("A Bool egy logikai típusú változó.\nMérete 1 byte.");
+                                Console.WriteLine("A logikai típus értéke a true vagy false értéket veheti fel.");
+                                Console.WriteLine("Deklarálása:\nbool (NÉV) = (true/false);");
+                                Console.WriteLine("PL:\nbool Boolean = true;\nBoolean = false;");
+                                break;
+                            }
+                            
+                        case 2://Numerical
+                            {
+                            A9:
+                                Console.Clear();
+                                Console.WriteLine("-C#-");
+                                Console.WriteLine("--Változók--");
+                                Console.WriteLine("---Numerical---");
+                                Console.WriteLine("Byte: 1");
+                                Console.WriteLine("Vissza lépés: 0");
+                                A9 = Convert.ToInt32(Console.ReadLine());
+
+                                switch (A9)
+                                {
+                                    case 1:
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Byte:\n"); Console.ResetColor();
+                                            Console.WriteLine("A Byte egy olyan szám tipusú változó amely 0 és 255 között képes tárolni számokat.\nMérete 1 byte");
+                                            Console.WriteLine("A Byte 1 byton az az 8 biten tárol csak pozitív számokat(0-255).");
+                                            Console.WriteLine("Deklarálása:\nbyte (NÉV) = (0-255);");
+                                            Console.WriteLine("PL:\nbyte Byte = 99;\nByte = 0;");
+                                            break;
+                                        }
+                                    case 0:
+                                        {
+                                            goto S0;
+                                        }
+                                }
+                                break;
+                            }
+                        case 3://betűs
+                            {
+                                A9:
+                                Console.Clear();
+                                Console.WriteLine("-C#-");
+                                Console.WriteLine("--Változók--");
+                                Console.WriteLine("---Betűs---");
+                                Console.WriteLine("Char: 1");
+                                Console.WriteLine("Vissza lépés: 0");
+                                A9 = Convert.ToInt32(Console.ReadLine());
+                                switch (A9)
+                                {
+                                    case 1://Char
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Char:\n"); Console.ResetColor();
+                                            Console.WriteLine("A Char egy karakter típusú változó.\nMérete 2 byte.");
+                                            Console.WriteLine("A char 16 bites karakterábrázolást (unikód) használ.");
+                                            Console.WriteLine("A backslash (\\) karakter speciális jelentéssel bír. Az utána következő karakter(eke)t, mint egy escape szekvenciát dolgozza föl a fordító.Az így használható escape szekvenciák a következők:");
+                                            Console.WriteLine("\\a - a 7-es kódú csipogás\n\\b - backspace, előző karakter törlése\n\\f - formfeed, soremelés karakter\n\\r - kocsi vissza karakter\n\\n - új sor karakter (soremelés + kocsi vissza)");
+                                            Console.WriteLine("Az új sor karakter hatása azonos a formfeed és a kocsi vissza karakterekhatásával");
+                                            Console.WriteLine("\\t - tabulátor karakter\n\\v - függőleges tabulátor\n\\\\ - backslash karakter\n\\' - aposztróf\n\\\" - idézőjel\n\\? - kérdőjel\n\\uxxyy xx és yy unikódú karakter");
+                                            Console.WriteLine("Deklarálása: \n char (NÉV) = '(BÁRMELY KARAKTER';");
+                                            Console.WriteLine("PL:\nchar Character = 'Z';\nCharacter = 'S';");
+                                            break;
+                                        }
+                                    case 0:
+                                        {
+                                            goto S0;
+                                        }
+                                }
+                                break;
+                            }
+                        case 0:
+                            {
+                                goto S1;
+                            }
                     }
                     break;
+                case 0:
+                    {
+                        goto S2;
+                    }
             }
 
 
             Console.ReadLine();
+        S2:;
         }
     }
 }
